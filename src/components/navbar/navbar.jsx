126b52342a9b2eea4add53897bd76ee2081bc363
MD HUSSAIN <mdhussain6291@gmail.com>
2026-02-10 17:46:09 +0530

    Updated pages and styles

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react"; // ✅ ADDED useState
 import { Link } from "react-router-dom";
 import "./navbar.css";
 import logo from "../../assets/logo.svg.png";
@@ -8,6 +8,8 @@ import Button from "../button/button";
 import { FiHome, FiUser, FiFolder, FiFileText } from "react-icons/fi";
 
 const Navbar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false); // ✅ ADDED state
 
   const handleConnect = () => {
     window.location.href = "mailto:mdhussain6291@gmail.com";
@@ -19,25 +21,33 @@ const Navbar = () => {
       {/* Logo */}
       <img src={logo} alt="Logo" className="nav-logo" />
 
+      {/* ✅ ADDED Mobile Menu Toggle */}
+      <div
+        className="menu-toggle"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+
       {/* Menu */}
-      <ul className="nav-menu">
+      <ul className={`nav-menu ${menuOpen ? "active" : ""}`}> {/* ✅ ADDED active class */}
         <li>
-          <Link to="/">
+          <Link to="/" onClick={() => setMenuOpen(false)}> {/* ✅ ADDED close on click */}
             <FiHome className="nav-icon" /> Home
           </Link>
         </li>
         <li>
-          <Link to="/about">
+          <Link to="/about" onClick={() => setMenuOpen(false)}>
             <FiUser className="nav-icon" /> About Me
           </Link>
         </li>
         <li>
-          <Link to="/projects">
+          <Link to="/projects" onClick={() => setMenuOpen(false)}>
             <FiFolder className="nav-icon" /> Projects
           </Link>
         </li>
         <li>
-          <Link to="/resume">
+          <Link to="/resume" onClick={() => setMenuOpen(false)}>
             <FiFileText className="nav-icon" /> Resume
           </Link>
         </li>
